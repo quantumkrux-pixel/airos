5e2c88d07a17deea6e5b4ef2cf75892f66b6cb75
quantumkrux-pixel <quantum.krux@gmail.com>
2026-01-16 02:47:13 -0500

    Rename Screengrip export to Screen Ripper

--- src/components/apps/index.js
+++ src/components/apps/index.js
@@ -7,6 +7,6 @@ export { default as Calendar } from './Calendar/Calendar';
 export { default as Terminal } from './Terminal/Terminal';
 export { default as CodeEditor } from './CodeEditor/CodeEditor';
 export { default as FileConverter } from './FileConverter/FileConverter';
-export { default as Screengrip } from './ScreenGrip';
+export { default as Screen Ripper } from './Screen Ripper';
 export { SketchApp } from './Sketch/SketchApp';
 export { AirChat } from './AirChat/AirChat';
