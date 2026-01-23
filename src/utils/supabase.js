eadbe6acc43a32685689bb882b00945b151070ad
Tyler <quantum.krux@gmail.com>
2026-01-23 06:45:57 -0500

    Delete src directory

--- src/utils/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://albdrcyjmfrbzbzopfpu.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsYmRyY3lqbWZyYnpiem9wZnB1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc1Mzk2MTAsImV4cCI6MjA4MzExNTYxMH0.M7SVuHxQCKu9nYeXTXvjEaFQ-HZhGzFy9TQCQu6Y-nU';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
